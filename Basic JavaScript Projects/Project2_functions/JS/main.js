1b9df242005d9571f33f58accf62854987b4a17d
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-08-05 16:29:19 -0700

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,10 @@
-function My_First_Function() {
-    var str = "This is the button text!";
-    document.getElementById("Button_Text").innerHTML = str;
+function My_First_Function() { //Creating a function
+    var str = "This is the button text!"; //I am creating a variable and giving it a value
+    document.getElementById("Button_Text").innerHTML = str; //The element with the ID Button_Text will display the string when the function is called
+}
+
+function myFunction() { //Creating a function
+    var sentence = "I am learning";
+     sentence += " a lot from this book!"; //Assigning variable "sentence" to string value, and concatenating the string
+    document.getElementById("Concatenate").innerHTML = sentence; //The element with the ID will display the concatenated string
 }
