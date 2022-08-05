55bef59f6e7aaed306c7ee49bdcd35e8863a4b84
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-08-05 15:14:14 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,25 @@
-alert("Hello, World!");
+window.alert("Hello, World!"); //This creates a popup window when loading the page
+
+document.write("Hello, World!"); //This creates text on the page
+
+var A = "This is a string"; //Here I am assigning the variable A a value
+
+document.write(A); //Here I am printing the value of A
+
+var X = "String Assignment";
+
+window.alert(X);
+
+document.write("This is a quote: \"That's crazy\""); //Here I am escaping characters
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ " -Dr. Seuss"); //Here I am concatenating multiple strings
+
+var B = "Concatenated" + " String"
+document.write(B);
+
+var Color = "Blue", Car = "Truck", Fruit = "Apple";
+document.write(Fruit); //Here I am assigning multiple variables within one statement
+
+document.write(3 + 3); //Here I am using an expression
